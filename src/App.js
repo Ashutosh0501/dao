1023d4f6d456c923ab621f7d1bf50266224f1977
Ashutosh Teotia <rooneyteotia@gmail.com>
2023-04-04 22:47:06 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -1,162 +1,309 @@
 import React, { useState, useEffect } from "react";
-import Web3,{utils} from "web3";
-import DAO from "./DAO.json";
+import Web3 from "web3";
 
-const App = () => {
-  const [accounts, setAccounts] = useState([]);
-  const [contract, setContract] = useState(null);
+const web3 = new Web3(Web3.givenProvider);
+
+const DAO_ADDRESS = "0xF2486518c6dC9C4EBA4e10f5884505Cea2A6d6Af"; // replace with the actual contract address
+const DAO_ABI = [
+  {
+    "inputs": [],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "inputs": [],
+    "name": "admin",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "applyForMembership",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_memberAddress",
+        "type": "address"
+      }
+    ],
+    "name": "approveMembership",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_proposalIndex",
+        "type": "uint256"
+      }
+    ],
+    "name": "closeProposal",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "_proposalText",
+        "type": "string"
+      }
+    ],
+    "name": "createProposal",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_proposalIndex",
+        "type": "uint256"
+      }
+    ],
+    "name": "getProposal",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      },
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "getProposalCount",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "members",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "isApproved",
+        "type": "bool"
+      },
+      {
+        "internalType": "bool",
+        "name": "hasVoted",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "proposals",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "proposalText",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "yesVotes",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "noVotes",
+        "type": "uint256"
+      },
+      {
+        "internalType": "bool",
+        "name": "isOpen",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_proposalIndex",
+        "type": "uint256"
+      },
+      {
+        "internalType": "bool",
+        "name": "_vote",
+        "type": "bool"
+      }
+    ],
+    "name": "vote",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  }
+];
+
+function App() {
+  const [isConnected, setIsConnected] = useState(false);
+  const [account, setAccount] = useState("");
   const [proposalText, setProposalText] = useState("");
   const [proposals, setProposals] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
-
+  const[aaddress, setaaddress]=useState("");
+  const proposalCount=0;
   useEffect(() => {
-    const init = async () => {
+    const connectToWeb3 = async () => {
       try {
-        const web3 = new Web3(window.ethereum);
-await window.ethereum.enable();
-const accounts = await web3.eth.getAccounts();
-setAccounts(accounts);
-
-       // const networkId = await web3.eth.net.getId();
-       // const deployedNetwork = DAO.networks[networkId];
-        const contract = new web3.eth.Contract(DAO.abi,"0x6524b09dc3ae3f980d47e16f8527153a0cac0c46" );
-        setContract(contract);
-
-        const proposalCount = await contract.methods.getProposalCount().call();
-        const proposals = [];
-        for (let i = 0; i < proposalCount; i++) {
-          const proposal = await contract.methods.getProposal(i).call();
-          proposals.push({
-            text: proposal[0],
-            yesVotes: proposal[1],
-            noVotes: proposal[2],
-            isOpen: proposal[3],
-          });
-        }
+        await window.ethereum.enable();
+        setIsConnected(true);
+        const accounts = await web3.eth.getAccounts();
+        setAccount(accounts[0]);
+        const daoContract = new web3.eth.Contract(DAO_ABI, DAO_ADDRESS);
+        const proposalCount = await daoContract.methods.getProposalCount().call();
+        const proposals = await Promise.all(
+          Array.from({ length: proposalCount }, (_, i) =>
+            daoContract.methods.getProposal(i).call()
+          )
+        );
         setProposals(proposals);
       } catch (error) {
         console.error(error);
-        setError(error.message);
       }
     };
-
-    init();
+    connectToWeb3();
   }, []);
 
-  const handleProposalTextChange = (event) => {
-    setProposalText(event.target.value);
-  };
-
   const handleApplyForMembership = async () => {
-    try {
-      setLoading(true);
-      await contract.methods.applyForMembership().send({ from: accounts[0] });
-      setLoading(false);
-    } catch (error) {
-      console.error(error);
-      setLoading(false);
-      setError(error.message);
-    }
-  };
-
-  const handleApproveMembership = async (memberAddress) => {
-    try {
-      setLoading(true);
-      await contract.methods.approveMembership(memberAddress).send({ from: accounts[0] });
-      setLoading(false);
-    } catch (error) {
-      console.error(error);
-      setLoading(false);
-      setError(error.message);
-    }
+    const daoContract = new web3.eth.Contract(DAO_ABI, DAO_ADDRESS);
+    await daoContract.methods.applyForMembership().send({ from: account });
   };
 
   const handleCreateProposal = async () => {
-    try {
-      setLoading(true);
-      await contract.methods.createProposal(proposalText).send({ from: accounts[0] });
-      setLoading(false);
-    } catch (error) {
-      console.error(error);
-      setLoading(false);
-      setError(error.message);
-    }
+    const daoContract = new web3.eth.Contract(DAO_ABI, DAO_ADDRESS);
+    await daoContract.methods.createProposal(proposalText).send({ from: account });
+    setProposalText("");
+    proposalCount++;
   };
 
   const handleVote = async (proposalIndex, vote) => {
-    try {
-      setLoading(true);
-      await contract.methods.vote(proposalIndex, vote).send({ from: accounts[0] });
-      setLoading(false);
-    } catch (error) {
-      console.error(error);
-      setLoading(false);
-      setError(error.message);
-    }
+    const daoContract = new web3.eth.Contract(DAO_ABI, DAO_ADDRESS);
+    await daoContract.methods.vote(proposalIndex, vote).send({ from: account });
+    const proposals = await Promise.all(
+      Array.from({ length: proposalCount }, (_, i) =>
+        daoContract.methods.getProposal(i).call()
+      )
+    );
+    setProposals(proposals);
+  };
+
+  const handleApproveMembership = async (memberAddress) => {
+    const daoContract = new web3.eth.Contract(DAO_ABI, DAO_ADDRESS);
+    await daoContract.methods.approveMembership(memberAddress).send({ from: account });
   };
 
   const handleCloseProposal = async (proposalIndex) => {
-    try {
-      setLoading(true);
-      await contract.methods.closeProposal(proposalIndex).send({ from: accounts[0] });
-      setLoading(false);
-    } catch (error) {
-      console.error(error);
-      setLoading(false);
-      setError(error.message);
-    }
+    const daoContract = new web3.eth.Contract(DAO_ABI, DAO_ADDRESS);
+    await daoContract.methods.closeProposal(proposalIndex).send({ from: account });
+    const proposals = await Promise.all(
+      Array.from({ length: proposalCount }, (_, i) =>
+        daoContract.methods.getProposal(i).call()
+      )
+    );
+    setProposals(proposals);
   };
+
+  if (!isConnected) {
+    return <div>Connecting to Web3...</div>;
+  }
 
   return (
     <div>
-      {error && <p>Error: {error}</p>}
-      <h1>DAO</h1>
+      <h1>DAO App</h1>
+      <div>Connected account: {account}</div>
+      <button onClick={handleApplyForMembership}>Apply for membership</button>
+      <h2>Create a proposal</h2>
       <div>
-        <h2>Membership</h2>
-        <p>Your account: {accounts[0]}</p>
-        <button onClick={handleApplyForMembership} disabled={loading}>
-          Apply for Membership
-        </button>
-        <h3>Members</h3>
-        <ul>
-          {accounts.map((account, index) => (
-            <li key={index}>{account}</li>
-          ))}
-        </ul>
+        <input type="text" value={proposalText} onChange={(e) => setProposalText(e.target.value)} />
+        <button onClick={handleCreateProposal}>Create</button>
       </div>
-      <div>
-        <h2>Proposals</h2>
-        <ul>
-          {proposals.map((proposal, index) => (
-            <li key={index}>
-              <p>{proposal.text}</p>
-              <p>Yes votes: {proposal.yesVotes}</p>
-              <p>No votes: {proposal.noVotes}</p>
-              {proposal.isOpen && (
-                <>
-                  <button onClick={() => handleVote(index, true)} disabled={loading}>
-                    Vote Yes
-                  </button>
-                  <button onClick={() => handleVote(index, false)} disabled={loading}>
-                    Vote No
-                  </button>
-                  <button onClick={() => handleCloseProposal(index)} disabled={loading}>
-                    Close Proposal
-                  </button>
-                </>
-              )}
-            </li>
-          ))}
-        </ul>
-        <div>
-          <h3>Create Proposal</h3>
-          <input type="text" value={proposalText} onChange={handleProposalTextChange} />
-          <button onClick={handleCreateProposal} disabled={loading}>
-            Create Proposal
-          </button>
-        </div>
-      </div>
-    </div>
-  );}
-export default App;  
+      <h2>Proposals</h2>
+      {proposals.map((proposal,index) => (
+<div key={index}>
+<h3>{proposal.title}</h3>
+<p>{proposal.description}</p>
+<p>Votes: {proposal.yesVotes} yes / {proposal.noVotes} no</p>
+<button onClick={() => handleVote(index, true)}>Vote yes</button>
+<button onClick={() => handleVote(index, false)}>Vote no</button>
+<button onClick={() => handleCloseProposal(index)}>Close proposal</button>
+</div>
+))}
+
+
+<ul>
+  <h2>approve Membership</h2>
+<input type="text" value={aaddress} onChange={(e) => setaaddress(e.target.value)} />
+       
+<button onClick={() => handleApproveMembership(aaddress)}>Approve</button>
+
+</ul>
+</div>
+);
+}
+
+export default App;
+
+
+
+
+
+
